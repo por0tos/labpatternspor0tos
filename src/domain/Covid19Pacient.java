2a0e0764308f704f68cc62ddac69f6eb315ee91f
nicoagr <github@nico.eus>
2023-10-27 10:09:56 +0200

    ejercicio 1 patron factory

--- src/domain/Covid19Pacient.java
+++ src/domain/Covid19Pacient.java
@@ -13,10 +13,13 @@ public class Covid19Pacient {
 	private String  name; 
 	private int age;
 	private Map<Symptom,Integer> symptoms=new HashMap<Symptom,Integer>();
+	private SymptomFactory ff;
 
-	public Covid19Pacient(String name, int years) {
+
+	public Covid19Pacient(String name, int years, SymptomFactory symptomFactory) {
 		this.name = name;
 		this.age = years;
+		ff = symptomFactory;
 	}
 	
 	public String getName() {
@@ -59,7 +62,7 @@ public class Covid19Pacient {
 	public Symptom addSymptomByName(String symptom, Integer w){
 		Symptom s=getSymptomByName(symptom);
 		if (s==null) {
-			s=createSymptom(symptom); 
+			s=ff.createSymptom(symptom); 
 			symptoms.put(s,w);		
 		}
 		return s;
@@ -92,32 +95,5 @@ public class Covid19Pacient {
 		return impact;
 	}
 	
-	private Symptom createSymptom(String symptomName) {
-	    List<String> impact5 = Arrays.asList("fiebre", "tos seca", "astenia","expectoracion");
-	    List<Double> index5 = Arrays.asList(87.9, 67.7, 38.1, 33.4);
-	    List<String> impact3 = Arrays.asList("disnea", "dolor de garganta", "cefalea","mialgia","escalofrios");
-	    List<Double> index3 = Arrays.asList(18.6, 13.9, 13.6, 14.8, 11.4);
-	    List<String> impact1 = Arrays.asList("nauseas", "vómitos", "congestión nasal","diarrea","hemoptisis","congestion conjuntival");
-	    List<Double> index1 = Arrays.asList(5.0, 4.8, 3.7, 0.9, 0.8);
-	    
-	    List<String> digestiveSymptom=Arrays.asList("nauseas", "vómitos","diarrea");
-	    List<String> neuroMuscularSymptom=Arrays.asList("fiebre", "astenia", "cefalea", "mialgia","escalofrios");
-	    List<String> respiratorySymptom=Arrays.asList("tos seca","expectoracion","disnea","dolor de garganta", "congestión nasal","hemoptisis","congestion conjuntival");
-
-
-	    int impact=0;
-	    double index=0;
-	    if (impact5.contains(symptomName)) {impact=5; index= index5.get(impact5.indexOf(symptomName));}
-	      else if (impact3.contains(symptomName)) {impact=3;index= index3.get(impact3.indexOf(symptomName));}
-	        else if (impact1.contains(symptomName)) {impact=1; index= index1.get(impact1.indexOf(symptomName));}
-	 
-	    if (impact!=0)  {
-	    	if (digestiveSymptom.contains(symptomName)) return new DigestiveSymptom(symptomName,(int)index, impact);
-	    	if (neuroMuscularSymptom.contains(symptomName)) return new NeuroMuscularSymptom(symptomName,(int)index, impact);
-	    	if (respiratorySymptom.contains(symptomName)) return new RespiratorySymptom(symptomName,(int)index, impact);
-	    }
-	    return null;		
-		
-	}
 }
 
--- src/factory/Main.java
+++ src/factory/Main.java
@@ -2,12 +2,15 @@ package factory;
 
 import domain.Covid19Pacient;
 import domain.Medicament;
+import domain.SymptomFactory;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
-		new PacientSymptomGUI(p1);
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35, new SymptomFactory());
+		PacientSymptomGUI pgui = new PacientSymptomGUI(p1);
+		Medicament m=new Medicament("Ibuprofeno", new SymptomFactory());
+		MedicalGUI mgui = new MedicalGUI(m);
 
 	}
 
